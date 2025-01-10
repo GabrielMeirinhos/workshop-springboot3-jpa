c6dabd85069661befa37db628e8d2b101036328f
Gabriel Meirinhos <gabriel.meirinhos3@gmail.com>
2025-01-10 15:12:17 -0300

    Project created

---
+++ main/java/com/kipper/frit_project_app/FritProjectAppApplication.java
@@ -0,0 +1,13 @@
+package com.kipper.frit_project_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FritProjectAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FritProjectAppApplication.class, args);
+	}
+
+}
---
+++ test/java/com/kipper/frit_project_app/FritProjectAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kipper.frit_project_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FritProjectAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
