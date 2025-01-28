4aff3d86ad5fe3365285ce5e9647b650621a8d5e
Gabriel Meirinhos <gabriel.meirinhos3@gmail.com>
2025-01-28 11:01:56 -0300

    H2 database, test profile, JPA

--- main/java/com/kipper/frit_project_app/entities/User.java
+++ main/java/com/kipper/frit_project_app/entities/User.java
@@ -1,14 +1,21 @@
 package com.kipper.frit_project_app.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serial;
 import java.io.Serializable;
 import java.util.Objects;
+import jakarta.persistence.Entity;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     @Serial
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
