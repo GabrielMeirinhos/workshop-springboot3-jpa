da5c60a7897da5f867c5e0d8549d168866c9fe27
Gabriel Meirinhos <gabriel.meirinhos3@gmail.com>
2025-01-28 19:13:24 -0300

    Service layer, component registration

---
+++ main/java/com/kipper/frit_project_app/service/UserService.java
@@ -0,0 +1,28 @@
+package com.kipper.frit_project_app.service;
+
+import com.kipper.frit_project_app.entities.User;
+import com.kipper.frit_project_app.repositories.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
