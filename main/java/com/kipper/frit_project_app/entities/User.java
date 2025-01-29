a8f2c257aeac868d09a4c02c2188e9c8921c6465
Gabriel Meirinhos <gabriel.meirinhos3@gmail.com>
2025-01-29 10:26:45 -0300

    Order, Instant, ISO 8601 - Part 1

--- main/java/com/kipper/frit_project_app/entities/User.java
+++ main/java/com/kipper/frit_project_app/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.*;
 
 import java.io.Serial;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 import jakarta.persistence.Entity;
 
@@ -21,6 +23,9 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {}
 
@@ -72,6 +77,10 @@ public class User implements Serializable {
         this.password = password;
     }
 
+    public List<Order> getOrders() {
+        return orders;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
@@ -83,4 +92,5 @@ public class User implements Serializable {
     public int hashCode() {
         return Objects.hashCode(id);
     }
+
 }
